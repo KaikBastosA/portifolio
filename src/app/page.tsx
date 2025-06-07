9c849eec4908e8bda54a41e99cf75d9951f72a03
Kaik <kaikbastos14@gmail.com>
2025-06-07 20:21:06 -0300

    Refact: refatorado o texto da página inicial e modificado o texto das habilidades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         <h3>Full Stack Developer</h3>
         <h1>Kaik Bastos</h1>
         <div className={style.amText}>
-          <p>Minha jornada como desenvolvedor começou cedo, quando percebi que podia transformar ideias em soluções reais através do código. Hoje, como desenvolvedor full-stack, trabalho tanto no frontend quanto no backend, garantindo que cada parte de um projeto seja funcional, intuitiva e eficiente.</p>
-          <p>Minha experiência varia desde aplicativos dinâmicos até plataformas robustas, sempre com foco em entregar qualidade e inovação. Sou movido pelo desafio de resolver problemas complexos com criatividade e lógica, e acredito que a tecnologia, quando bem utilizada, pode mudar realidades.</p>
+            <p>Minha jornada como desenvolvedor começou com a paixão por transformar ideias em realidade através do código. Hoje, como especialista em front-end, dedico-me a construir interfaces de usuário responsivas, performáticas e visualmente impactantes.</p>
+            <p>Minha experiência é focada em traduzir designs em aplicações interativas e acessíveis, garantindo sempre uma experiência de usuário de alta qualidade. Sou movido pelo desafio de resolver problemas complexos com criatividade e lógica, acreditando que a tecnologia, através de um front-end bem executado, tem o poder de criar conexões reais.</p>
         </div>
         <BotaoBranco texto="Mais sobre mim" link="/" />
         <SocialMedia />
